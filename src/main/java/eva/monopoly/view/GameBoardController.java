65e23e9a36365bbb02505c4c49977140b8107095
Philipp Spandl <ph.spandl@gmx.de>
2019-01-13 18:23:16 +0100

    More View funcitonality
    Added functionality mainly for navigating between scenes

--- src/main/java/eva/monopoly/view/GameBoardController.java
+++ src/main/java/eva/monopoly/view/GameBoardController.java
@@ -32,4 +32,12 @@ public class GameBoardController {
 	private void showNickname(Player player) {
 		playerName.setText(player.getName());
 	}
+
+	public void showRoundData() {
+		/*
+		 * show Dice roll, show data of target field, give button options to buy
+		 * or ignore if field is not occupied, give option to place house if
+		 * target field and corresponding street is owned by the player
+		 */
+	}
 }
--- src/main/java/eva/monopoly/view/StartupController.java
+++ src/main/java/eva/monopoly/view/StartupController.java
@@ -1,16 +1,31 @@
 package eva.monopoly.view;
 
+import java.io.IOException;
+
 import eva.monopoly.game.player.Player;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class StartupController {
 	@FXML
 	TextField nickname;
 	@FXML
 	Button okBttn;
-	private void enterNickname() {
+	public void enterNickname(ActionEvent event) throws IOException {
 		// set Playername from Textfield nickname and switch to mainMenu.fxml
+		nickname.getText();
+		Parent mainMenuParent = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+		Scene mainMenu = new Scene(mainMenuParent);
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(mainMenu);
+		window.show();
 	}
 }
+
